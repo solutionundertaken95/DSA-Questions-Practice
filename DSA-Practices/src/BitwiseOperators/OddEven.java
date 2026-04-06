ba5e162b8b18b620ca1c621b25e9781ae42b49a4
Gokul S Ganesh <gokulsg02@gmail.com>
2026-04-06 23:30:15 +0530

    OddEven using Bitwise Operator

---
+++ DSA-Practices/src/BitwiseOperators/OddEven.java
@@ -0,0 +1,14 @@
+package BitwiseOperators;
+
+public class OddEven {
+    public static void main(String[] args) {
+        int n = 68;
+
+        System.out.println(isOdd(n));
+    }
+
+    private static boolean isOdd(int n) {
+
+        return (n & 1) == 1;
+    }
+}
