9d6b8cbe7968f2f8f9b18d63d1e509828caa9235
Gokul S Ganesh <gokulsg02@gmail.com>
2026-04-18 18:21:08 +0530

    Numpad Leetcode Question and return arraylist  using recursion

--- DSA-Practices/src/Recursion/PhonePad.java
+++ DSA-Practices/src/Recursion/PhonePad.java
@@ -1,9 +1,13 @@
 package Recursion;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class PhonePad {
     public static void main(String[] args) {
         String str = "19";
-        numpad("",str);
+        ArrayList<String> arrayList = numpad2("",str);
+        System.out.println(arrayList);
     }
 
     private static void numpad(String processed, String unprocessed) {
@@ -26,6 +30,35 @@ public class PhonePad {
             numpad(processed + ch, unprocessed.substring(1));
 
         }
+
+    }
+
+
+    private static ArrayList<String> numpad2(String processed, String unprocessed) {
+
+        if(unprocessed.isEmpty()){
+            ArrayList<String> stringArrayList = new ArrayList<>();
+            stringArrayList.add(processed);
+            return stringArrayList;
+        }
+
+        int digit = unprocessed.charAt(0) - '0';
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        for (int i = (digit-1) * 3; i < digit*3; i++) {
+
+            char ch = (char) ('a' + i);
+
+            if(i==25){
+                i = i +1;
+            }
+
+          arrayList.addAll(numpad2(processed + ch, unprocessed.substring(1)));
+
+        }
+
+        return arrayList;
 
     }
 }
