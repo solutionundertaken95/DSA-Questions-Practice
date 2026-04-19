015f8794db0eecc73a3d7823efb0d5c3bf2d626b
Gokul S Ganesh <gokulsg02@gmail.com>
2026-04-19 15:04:14 +0530

    Maze Question

---
+++ DSA-Practices/src/BackTracking/Maze.java
@@ -0,0 +1,23 @@
+package BackTracking;
+
+public class Maze {
+    public static void main(String[] args) {
+        int count = maze(3,3);
+        System.out.println(count);
+    }
+
+    private static int maze(int r, int c) {
+
+        if(r == 1 || c == 1){
+            return 1;
+        }
+
+        int left = maze(r-1,c);
+
+        int right = maze(r,c-1);
+
+        return left+right;
+
+    }
+
+}
