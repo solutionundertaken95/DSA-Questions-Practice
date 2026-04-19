3651be306eb8885b4121efc44a46c3a0ca85ed04
Gokul S Ganesh <gokulsg02@gmail.com>
2026-04-19 15:27:02 +0530

    Printing the paths of the maze

--- DSA-Practices/src/BackTracking/Maze.java
+++ DSA-Practices/src/BackTracking/Maze.java
@@ -4,6 +4,8 @@ public class Maze {
     public static void main(String[] args) {
         int count = maze(3,3);
         System.out.println(count);
+
+        mazeprint("",3,3);
     }
 
     private static int maze(int r, int c) {
@@ -17,6 +19,23 @@ public class Maze {
         int right = maze(r,c-1);
 
         return left+right;
+
+    }
+
+    private static void mazeprint(String p, int r, int c) {
+
+        if(r == 1 && c == 1){
+            System.out.println(p);
+            return;
+        }
+
+        if(r > 1){
+            mazeprint(p + 'D',r-1,c);
+        }
+
+        if(c > 1){
+            mazeprint(p + 'R',r,c-1);
+        }
 
     }
 
