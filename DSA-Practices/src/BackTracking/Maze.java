3046b40bcc06a48032b99f3bb3a3c5135af8e710
Gokul S Ganesh <gokulsg02@gmail.com>
2026-04-19 15:30:37 +0530

    Printing the paths of the maze and adding to arraylist

--- DSA-Practices/src/BackTracking/Maze.java
+++ DSA-Practices/src/BackTracking/Maze.java
@@ -1,11 +1,15 @@
 package BackTracking;
 
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args) {
         int count = maze(3,3);
         System.out.println(count);
 
         mazeprint("",3,3);
+
+        System.out.println(mazeprintRet("",3,3));
     }
 
     private static int maze(int r, int c) {
@@ -36,6 +40,30 @@ public class Maze {
         if(c > 1){
             mazeprint(p + 'R',r,c-1);
         }
+
+    }
+
+
+    private static ArrayList<String> mazeprintRet(String p, int r, int c) {
+
+        if(r == 1 && c == 1){
+            ArrayList<String> arrayList = new ArrayList<>();
+            arrayList.add(p);
+            return arrayList;
+        }
+
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if(r > 1){
+            list.addAll(mazeprintRet(p + 'D',r-1,c));
+        }
+
+        if(c > 1){
+            list.addAll(mazeprintRet(p + 'R',r,c-1));
+        }
+
+        return list;
 
     }
 
